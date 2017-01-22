041ab797015955caff56fd0729dde6745d8301af
mmartsen <bluesmix@gmail.com>
2017-01-22 05:10:34 -0800

    Adjust Solr example. Add comments for Impala

---
+++ impala/src/main/java/impala/App.java
@@ -0,0 +1,17 @@
+package impala;
+
+/**
+ * There is an official guide: http://www.cloudera.com/documentation/other/connectors/impala-jdbc/latest.html
+ * 
+ * And unofficial way to do with custom maven repo: https://github.com/onefoursix/Cloudera-Impala-JDBC-Example
+ * 
+ * After adding all those jars to a classpath in one or another way it can be used as usual JDBC app
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
--- solr/src/main/java/org/hadoop/examples/solr/SolrSampleApp.java
+++ solr/src/main/java/org/hadoop/examples/solr/SolrSampleApp.java
@@ -4,8 +4,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
+import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrInputDocument;
 
 
@@ -13,6 +15,11 @@ import org.apache.solr.common.SolrInputDocument;
  * 
  * Check the http://quickstart.cloudera:8983 url to see the SOLR web ui. 
  * I will use pre-existing cloudera example data, but is also possible to create a new core
+ * 
+ * Useful links: http://stackoverflow.com/questions/35603902/how-to-start-using-apache-solr-with-java-and-run-simple-queries-without-running
+ * https://cwiki.apache.org/confluence/display/solr/Using+SolrJ
+ * http://lucene.apache.org/solr/6_3_0/quickstart.html
+ * https://cwiki.apache.org/confluence/display/solr/Searching
  *
  */
 public class SolrSampleApp {
@@ -20,7 +27,9 @@ public class SolrSampleApp {
 
 	public static void main(String[] args) throws SolrServerException, IOException {
 		SolrSampleApp solr = new SolrSampleApp();
-		solr.addDoc();
+		//solr.addDoc();
+		solr.printSchema();
+		solr.selectUsers();
 	}
 
 	public SolrSampleApp() {
@@ -34,12 +43,27 @@ public class SolrSampleApp {
 	public void addDoc() throws SolrServerException, IOException {
 		SolrInputDocument doc1 = new SolrInputDocument();
 		doc1.addField("id", 123, 1.0f);
-		doc1.addField("username", "test_name", 1.0f);
-		doc1.addField("price", 10);
+		doc1.addField("user_name", "test_name", 1.0f);
 
 		Collection<SolrInputDocument> docs = new ArrayList<SolrInputDocument>();
 		docs.add(doc1);
 		server.add(docs);
 		server.commit();
+	}
+	
+	public void printSchema() throws SolrServerException {
+		SolrQuery query = new SolrQuery();
+		query.setRequestHandler("/schema/fields");
+		query.setParam("showDefaults", true);
+		QueryResponse response = server.query(query);
+		System.out.println(response.getResponse());
+	}
+	
+	public void selectUsers() throws SolrServerException {
+		SolrQuery query = new SolrQuery();
+		query.setFields("user_name", "text"); //set fields to retrieve
+		query.setQuery("user_location:Malaysia OR user_location:London"); //set search conditions
+		QueryResponse response = server.query(query);
+		System.out.println(response.getResults());
 	}
 }
